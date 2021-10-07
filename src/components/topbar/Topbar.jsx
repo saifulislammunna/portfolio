cb65a0cd1f5382503cc80e9fd81edc35e03f64f1
saifulislammunna <saifulmunna28@gmail.com>
2021-10-07 14:36:10 +0600

    top-bar-added

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import './topbar.scss'
+import {Person,Mail} from "@material-ui/icons"
 const Topbar = () => {
     return (
         <div className="topbar">
              <div className="wrapper">
                  <div className="left">
-                     <a href="#intro" className="logo">   saiful islam </a>
+                     <a href="#intro" className="logo">   saiful. </a>
+                     <div className="item-container">
+                        <Person className="icon"/>
+                        <span>+8801799****88</span>
+                     </div>
+                     <div className="item-container">
+                        <Mail className="icon"/>
+                        <span>saifulmunna**@gmail.com</span>
+                     </div>
                       
                  </div>
                 
                  <div className="right">
-                    this is right
+                   
                  </div>
              </div>
         </div>
