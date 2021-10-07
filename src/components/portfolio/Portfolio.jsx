2428e473be43cbe22b492c12f9666c6eb947f106
saifulislammunna <saifulmunna28@gmail.com>
2021-10-07 23:49:56 +0600

    html-added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -4,6 +4,20 @@ const Portfolio = () => {
     return (
         <div  className="portfolio" id="portfolio">
             <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Html</li>
+                <li>Css</li>
+                <li>JavaScript</li>
+                <li>React js </li>
+                <li>Node js</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="assets/html51.jpg " alt="" />
+                    <h3>Html</h3>
+                </div>
+            </div>
         </div>
     );
 };
