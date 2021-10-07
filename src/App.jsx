0e82cf754c184183c287fa4085d7f20b246e58eb
saifulislammunna <saifulmunna28@gmail.com>
2021-10-07 16:20:19 +0600

    topbar-added

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,15 @@ import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
  import "./app.scss"
+import { useState } from "react";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(true);
   return (
     <div className="app">
        {/* top
        sec -intro -portfolio -work ... */}
-       <Topbar/>
+       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
         <div className="sections">
    
            <Intro/>
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './topbar.scss'
 import {Person,Mail} from "@material-ui/icons"
-const Topbar = () => {
+export default function Topbar({menuOpen, setMenuOpen}){
     return (
-        <div className="topbar">
+        <div className={"topbar " + (menuOpen && "active")}>
              <div className="wrapper">
                  <div className="left">
                      <a href="#intro" className="logo">   saiful. </a>
@@ -19,11 +19,15 @@ const Topbar = () => {
                  </div>
                 
                  <div className="right">
-                   
+                   <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+                       <span className="line1"></span>
+                       <span className="line2"></span>
+                       <span className="line3"></span>
+                   </div>
                  </div>
              </div>
         </div>
     );
 };
 
-export default Topbar;
+ 
