31c60c3f9478c599a57ff0dad0ca6a014894ec8d
saifulislammunna <saifulmunna28@gmail.com>
2021-10-08 11:59:13 +0600

    works-added

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+ 
 import './works.scss'
-const Works = () => {
+ export default function Works() {
     return (
         <div className="works" id="works"> 
-   
+                 <div className="slider">
+                     <div className="container">
+                         <div className="item">
+                             <div className="left">
+                                 <div className="leftContainer">
+                                     <div className="imgContainer">
+                                         <img src="assets/images.jpg" alt="" />
+                                     </div>
+                                     <h2>Plane Html & css</h2>
+                                     <p>order different types of recipes</p>
+                                     <span>projects</span>
+                                 </div>
+                             </div>
+                             <div className="right">
+                                       <img src="assets/Food network.jpg" alt="" />
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+                 <img src="assets/arrow.png" className="arrow left" alt="" />
+                 <img src="assets/arrow.png" className="arrow right" alt="" />
         </div>
     );
 };
 
-export default Works;
+ 
