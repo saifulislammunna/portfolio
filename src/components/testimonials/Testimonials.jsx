c63abe70318f630d06fa498682e37457fa39702c
saifulislammunna <saifulmunna28@gmail.com>
2021-10-08 15:06:16 +0600

    testimonial-added

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,11 +1,31 @@
-import React from 'react';
+ 
 import './testimonials.scss'
-const Testimonials = () => {
+ 
+
+export default function Testimonials(){
     return (
-        <div className="testimonials" id="testimonials">
-      
-        </div>
+    <div className="testimonials" id="testimonials">
+            <h1>Testimonials</h1>
+            <div className="container">
+                <div className="card">
+                    <div className="top">
+                        <img src="assets/right-arrow.png" className="left" alt="" />
+                        <img className="user
+                        " src="assets/user-1.png" alt="" />
+                        <img className="right" src="assets/Youtube.png" alt="" />
+                         </div>
+                        <div className="center">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, voluptatibus.
+                        </div>
+                        
+                       <div className="bottom">
+                                <h3>Ibrahim</h3>
+                                <h4>CEO of LAMA</h4>
+                      </div>
+                        
+                   
+                </div>
+            </div>
+    </div>
     );
 };
-
-export default Testimonials;
