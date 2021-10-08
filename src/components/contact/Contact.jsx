bf2941e47549de6b7cf8de42a4c44001d724c4f3
saifulislammunna <saifulmunna28@gmail.com>
2021-10-08 15:42:47 +0600

    contact-added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+ 
+import React, { useState } from 'react';
 import './contact.scss'
 const Contact = () => {
+   
+    const [message,setMessage] = useState(false)
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        setMessage(true)
+    }
     return (
         <div className="contact" id="contact">
-           
+           <div className="left">
+                 <img src="assets/shake3.png" alt="" />
+           </div>
+           <div className="right">
+               <h2>Contact.</h2>
+               <form onSubmit={handleSubmit}>
+                   <input type="text" placeholder="Email" />
+                   <textarea placeholder="Message"></textarea>
+                   <button type="submit">Send</button>
+                   {message && <span>Thanks, I'll reply ASAP:)</span>}
+               </form>
+           </div>
         </div>
     );
 };
